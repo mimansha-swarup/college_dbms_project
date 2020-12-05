ca3fbafedb00057b94de07957076fd30ef083622
mimanhsa-swarup <mimanhsaswarup@gmail.com>
2020-12-05 18:40:59 +0530

    studebt route add

--- routes/auth.js
+++ routes/auth.js
@@ -12,5 +12,8 @@ router.post('/getDetails', authController.getDetails);
 router.post('/remove', authController.remove);
 router.post('/addLink', authController.addLink);
 router.post('/getLog', authController.getLog);
+router.post('/viewDetails', authController.viewDetails);
+router.post('/teacherDetails', authController.teacherDetails);
+
 
 module.exports = router;
